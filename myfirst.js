a9113903e95e63ded6dc5d5d5612c17361cc6e82
Karan sapra <88654249+CraveforSuccess@users.noreply.github.com>
2023-01-06 16:13:00 +0530

    Add files via upload

--- myfirst.js
+++ myfirst.js
@@ -1,29 +1,55 @@
-// 03-January-2023
+// 5 January 2023
+// Upload the content
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+var url = require('url')
 
-http.createServer(function (req, res) {
-  if(req.url=='/fileupload'){
-    var form = new formidable.IncomingForm();
-    form.parse(req,function(err,fields,files){
-      var oldpath = files.filetoupload.filepath;
-      var newpath = 'C:/Users/Karan/' + files.filetoupload.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
+http.createServer((req, res) => {
+ 
 
+ 
+  if(req.url!='/fileupload') {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename,(err,data)=>{
+      if(err){
+        res.writeHead(404,{"Content-type":"text/html"});
+        return res.end("404 File Not Found")
+      }else{
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.write(data)
+        return res.end();
+      }
     })
-  }else{
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
   }
+  else {
+    if(req.url=='/fileupload'){
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.filepath;
+        var newpath = 'C:/Users/karan/' + files.filetoupload.originalFilename;
+        fs.rename(oldpath, newpath, function (err) {
+          if (err) throw err;
+          res.write('File uploaded and moved!');
+          res.end();
+        });
+      });
+    }
+    else{
+      window.location.reload()
+    }
 
-}).listen(8080);
+     
+  }
+  
+
+
+
+
+
+
+
+
+
+}).listen(8080)
